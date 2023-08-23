d803862b915a2f44e0a5486f4ebbfc191ea354ca
Zwylair <82669735+Zwylair@users.noreply.github.com>
2023-08-23 16:46:57 +0300

    y

--- settings.py
+++ settings.py
@@ -1,3 +1,3 @@
 import os
 
-PZ_MODS_PATH = f'mods'  # f'{os.getenv("userprofile")}\\Zomboid\\mods'   
+PZ_MODS_PATH = f'{os.getenv("userprofile")}\\Zomboid\\mods'  # f'{os.getenv("userprofile")}\\Zomboid\\mods'
