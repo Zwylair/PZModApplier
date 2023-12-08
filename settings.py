f8134d7f624111c677e5608c3769a438a055afb9
Zwylair <82669735+Zwylair@users.noreply.github.com>
2023-12-09 01:41:11 +0200

    build corrections

--- settings.py
+++ settings.py
@@ -1,3 +1,8 @@
 import os
 
 PZ_MODS_PATH = f'{os.getenv("userprofile")}\\Zomboid\\mods'  # f'{os.getenv("userprofile")}\\Zomboid\\mods'
+
+# setup settings
+BUILT_ARCHIVE_NAME = 'built.7z'
+ICON_PATH = 'icon.ico'
+IS_GUI = True
